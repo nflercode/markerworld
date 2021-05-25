087a36e04a103f036a90b651ec86cbe20a51b123
Isaac Pieber <isaacpieber96@gmail.com>
2021-05-25 23:24:20 +0200

    danger

--- src/repositories/rdb.js
+++ src/repositories/rdb.js
@@ -3,10 +3,12 @@ import r from 'rethinkdb';
 async function connect() {
   let connection;
   try {
+    console.log('pw:', process.env.DB_PASSWORD);
     connection = await r.connect({ host: '10.245.135.3', port: 28015, db: 'nfler_db', password: process.env.DB_PASSWORD, user: 'admin' });
     console.log('Successfully connected to db');
   } catch (err) {
     console.error('Failed to connect to db', err);
+    return;
   }
 
   r.dbList().contains('nfler_db').do(exists =>
